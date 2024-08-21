687605c0fbce8619e27f29e5ed86a5ff3fd30995
lifuhuang97 <lifuhuang97@gmail.com>
2024-08-21 17:03:14 +0800

    removed outdated test pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import { Toaster } from '@/components/ui/toaster'
 import '@mantine/core/styles.css'
 import { Route, Routes } from 'react-router-dom'
 import RootLayout from './_root/RootLayout'
-import {
-  LandingPage,
-  // LyricsTestPage,
-  PlayerPage,
-  TestGeneratePage,
-  TestRequestPage,
-  TestStreamPage,
-  TestUploadPage,
-} from './_root/pages'
-import TestLyricsPage from './_root/pages/TestLyricsPage'
+import { LandingPage, PlayerPage } from './_root/pages'
 import './globals.css'
 
 const App = () => {
@@ -21,11 +12,11 @@ const App = () => {
         <Route element={<RootLayout />}>
           <Route index element={<LandingPage />} />
           <Route path='/v/:videoId' element={<PlayerPage />} />
-          <Route path='/test' element={<TestGeneratePage />} />
-          <Route path='/test2' element={<TestStreamPage />} />
-          <Route path='/test_upload' element={<TestUploadPage />} />
-          <Route path='/test_lyrics' element={<TestLyricsPage />} />
-          <Route path='/test_request' element={<TestRequestPage />} />
+          {/* <Route path='/test' element={<TestGeneratePage />} /> */}
+          {/* <Route path='/test2' element={<TestStreamPage />} /> */}
+          {/* <Route path='/test_upload' element={<TestUploadPage />} /> */}
+          {/* <Route path='/test_lyrics' element={<TestLyricsPage />} /> */}
+          {/* <Route path='/test_request' element={<TestRequestPage />} /> */}
           {/* <Route
             path='/lyricstest'
             element={
