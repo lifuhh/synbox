d626f5fff6c4d7a3af71cfd14f8881ef6d1f90cd
lifuhh <lifuhuang97@gmail.com>
2025-04-28 15:52:44 +0800

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,17 +9,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     sourcemap: true,
     rollupOptions: {
-      output: {
-        manualChunks: {
-          // Explicitly define chunks for external dependencies
-          'react-vendor': ['react', 'react-dom'],
-          'youtube': ['react-youtube'], // If you're using react-youtube
-        },
-        // Ensure proper asset file names
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
       onLog(level, log, handler) {
         if (
           log.cause &&
@@ -38,11 +27,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-  },
-  optimizeDeps: {
-    include: ['react-youtube'], // If you're using react-youtube
-    exclude: [], // Add any packages that should not be pre-bundled
   },
 })
